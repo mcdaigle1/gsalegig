d320b14b09fd259c1b800ac9ae46dbca3ee483c0
Michael Daigle <michaeldaigle@MacBook-Pro-6.attlocal.net>
2025-06-23 11:57:39 -0700

    move back to python application

---
+++ services/gsalegig-api/app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+import requests
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Define a route for the root URL ("/")
+@app.route('/')
+def hello_world():
+    response = requests.get("http://name-generator-app-service/")
+    name = response.text
+    
+    return 'Hello, World!' + name
+
+# Run the app if this script is executed
+if __name__ == '__main__':
+    # Start the Flask web server
+    app.run(debug=True, port=8080)
