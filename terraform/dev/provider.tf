abd76d39af9a50482298fdd9182d1f626679a450
Michael Daigle <michaeldaigle@MacBook-Pro-6.attlocal.net>
2025-06-23 18:32:47 -0700

    rm clusters

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -7,7 +7,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0"
+      version = "~> 5.44"
      }
   }
+
+  required_version = ">= 1.3"
 }
