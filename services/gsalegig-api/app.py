d112b6d8658bdde5ff6c22b73bebd7c089344934
Michael Daigle <michaeldaigle@MacBook-Pro-6.attlocal.net>
2025-06-25 08:49:07 -0700

    don't call name service

--- services/gsalegig-api/app.py
+++ services/gsalegig-api/app.py
@@ -8,10 +8,11 @@ app = Flask(__name__)
 # Define a route for the root URL ("/")
 @app.route('/')
 def hello_world():
-    response = requests.get("http://name-generator-app-service/")
-    name = response.text
+    # response = requests.get("http://name-generator-app-service/")
+    # name = response.text
     
-    return 'Hello, World!' + name
+    # return 'Hello, World!' + name
+    return 'Hello, World!'
 
 # Run the app if this script is executed
 if __name__ == '__main__':
