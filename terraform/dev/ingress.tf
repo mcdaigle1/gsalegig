36760b3621af8c64815a230691dc7ed2729a3624
Michael Daigle <michaeldaigle@MacBook-Pro-6.attlocal.net>
2025-06-24 22:52:52 -0700

    add ingress

---
+++ terraform/dev/ingress.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "nginx_ingress" {
+  name       = "ingress-nginx"
+  namespace  = "dev"
+  create_namespace = true
+
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version    = "4.10.1" # use latest compatible with your k8s
+
+  values = [file("${path.module}/nginx-values.yaml")]
+}
--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -17,3 +17,9 @@ terraform {
 provider "kubernetes" {
   config_path = var.kubeconfig_path
 }
+
+provider "helm" {
+  kubernetes {
+    config_path = var.kubeconfig_path
+  }
+}
