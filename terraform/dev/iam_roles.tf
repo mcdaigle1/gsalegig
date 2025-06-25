99059539ff7c3537e57e84360d3df76635022b84
Michael Daigle <michaeldaigle@MacBook-Pro-6.attlocal.net>
2025-06-24 17:41:10 -0700

    start to replace flux CD with ArgoCD

--- terraform/dev/iam_roles.tf
+++ terraform/dev/iam_roles.tf
@@ -30,51 +30,51 @@ resource "aws_iam_role_policy_attachment" "cni_policy" {
   role       = aws_iam_role.eks_node_role.name
 }
 
-# IAM role for github actions
-resource "aws_iam_role" "github_actions_role" {
-  name = "github-actions-ecr-push"
+# # IAM role for github actions
+# resource "aws_iam_role" "github_actions_role" {
+#   name = "github-actions-ecr-push"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Federated = aws_iam_openid_connect_provider.github.arn
-      },
-      Action = "sts:AssumeRoleWithWebIdentity",
-      Condition = {
-        StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:mcdaigle1/gsalegig:ref:refs/heads/main"
-        }
-      }
-    }]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [{
+#       Effect = "Allow",
+#       Principal = {
+#         Federated = aws_iam_openid_connect_provider.github.arn
+#       },
+#       Action = "sts:AssumeRoleWithWebIdentity",
+#       Condition = {
+#         StringLike = {
+#           "token.actions.githubusercontent.com:sub" = "repo:mcdaigle1/gsalegig:ref:refs/heads/main"
+#         }
+#       }
+#     }]
+#   })
+# }
 
-resource "aws_iam_policy" "ecr_push" {
-  name = "GitHubActionsECRPush"
+# resource "aws_iam_policy" "ecr_push" {
+#   name = "GitHubActionsECRPush"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect   = "Allow",
-      Action   = [
-        "ecr:GetAuthorizationToken",
-        "ecr:BatchCheckLayerAvailability",
-        "ecr:PutImage",
-        "ecr:InitiateLayerUpload",
-        "ecr:UploadLayerPart",
-        "ecr:CompleteLayerUpload"
-      ],
-      Resource = "*"
-    }]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [{
+#       Effect   = "Allow",
+#       Action   = [
+#         "ecr:GetAuthorizationToken",
+#         "ecr:BatchCheckLayerAvailability",
+#         "ecr:PutImage",
+#         "ecr:InitiateLayerUpload",
+#         "ecr:UploadLayerPart",
+#         "ecr:CompleteLayerUpload"
+#       ],
+#       Resource = "*"
+#     }]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "attach" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = aws_iam_policy.ecr_push.arn
-}
+# resource "aws_iam_role_policy_attachment" "attach" {
+#   role       = aws_iam_role.github_actions_role.name
+#   policy_arn = aws_iam_policy.ecr_push.arn
+# }
 
 resource "aws_iam_role" "eks_cluster_role" {
   name = "gsalegig_eks_cluster-cluster-20250529174826744800000001"
