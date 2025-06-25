9002232908132f460777c75e756211286a716099
Michael Daigle <michaeldaigle@MacBook-Pro-6.attlocal.net>
2025-06-25 01:05:11 -0700

    change ingress namespace

--- terraform/dev/ingress.tf
+++ terraform/dev/ingress.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "nginx_ingress" {
   name       = "ingress-nginx"
-  namespace  = "dev"
+  namespace  = "ingress-nginx"
   create_namespace = true
 
   repository = "https://kubernetes.github.io/ingress-nginx"
