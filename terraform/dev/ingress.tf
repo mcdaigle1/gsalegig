2dfc2691487f0a017239dd227585c43b2e7c116b
Michael Daigle <michaeldaigle@MacBook-Pro-6.attlocal.net>
2025-06-25 16:32:31 -0700

    remove enable-prometheus-metrics

--- terraform/dev/ingress.tf
+++ terraform/dev/ingress.tf
@@ -3,9 +3,12 @@ resource "helm_release" "nginx_ingress" {
   namespace  = "ingress-nginx"
   create_namespace = true
 
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  version    = "4.12.3" # use latest compatible with your k8s
+  repository   = "https://kubernetes.github.io/ingress-nginx"
+  chart        = "ingress-nginx"
+  version      = "4.12.3" # use latest compatible with your k8s
+  timeout      = 600 # 10m
+  wait         = true
+  force_update = true
 
   values = [file("${path.module}/nginx-values.yaml")]
 }
