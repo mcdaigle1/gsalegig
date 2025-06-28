ec148d94e759cc9d578c105c8cdbbc1fd337b747
Michael Daigle <michaeldaigle@MacBook-Pro-6.attlocal.net>
2025-06-28 09:59:59 -0700

    add opensearch

---
+++ terraform/dev/opensearch.tf
@@ -0,0 +1,48 @@
+resource "aws_opensearch_domain" "jaeger" {
+  domain_name           = "jaeger-logs"
+  engine_version        = "OpenSearch_2.11"
+
+  cluster_config {
+    instance_type = "t3.small.search"
+    instance_count = 2
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_type = "gp3"
+    volume_size = 20
+  }
+
+  access_policies = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "es:*"
+      Resource  = "arn:aws:es:${var.region}:${data.aws_caller_identity.current.account_id}:domain/jaeger-logs/*"
+    }]
+  })
+
+  advanced_options = {
+    "rest.action.multi.allow_explicit_index" = "true"
+  }
+
+  node_to_node_encryption {
+    enabled = true
+  }
+
+  encrypt_at_rest {
+    enabled = true
+  }
+}
+
+resource "kubernetes_config_map" "jaeger_config" {
+  metadata {
+    name      = "jaeger-opensearch"
+    namespace = "monitoring"
+  }
+
+  data = {
+    OPENSEARCH_URL = aws_opensearch_domain.jaeger.endpoint
+  }
+}
